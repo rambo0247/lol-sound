8d54d1ca5198943d4b51b996e426249d344ebaa6
Leinier Perez <P_leinier@yahoo.com>
2026-01-11 21:32:37 -0500

    add google adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-8127047951510339" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
